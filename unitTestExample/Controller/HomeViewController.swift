e8fa59ed98c8c75fa40c3be3efeee42d63c11d77
Mohamed Sayed <mrdiscover22@gmail.com>
2021-03-29 20:21:03 +0200

    edit commit
    edit commit

--- unitTestExample/Controller/HomeViewController.swift
+++ unitTestExample/Controller/HomeViewController.swift
@@ -9,20 +9,27 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
+    //MARK:-> Outlet
     @IBOutlet weak var emailLabel: UILabel!
     @IBOutlet weak var passwordLabel: UILabel!
     
     
+    //MARK:-> Proprties
+
     var emailText = ""
     var passwordText = ""
     
+    //MARK:-> Life Cycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         setupReciveDataProprties()
 
     }
     
+    //MARK:-> Class Methods
+
     func setupReciveDataProprties() {
         emailLabel.text = emailText
         passwordLabel.text = passwordText
